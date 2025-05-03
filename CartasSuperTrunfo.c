a0384ec021b41c8e69e22a1215e904e7442c62b4
jonatasdaguerre <jonatasdaguerre@gmail.com>
2025-05-03 15:53:58 +0000

    Alterações pontuais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    char estado[3], codigocarta[10], cidade[50];
-    int populacao, pib, pontosturisticos;
-    float areakm2;
+    //Declaração das variáveis da Carta 1
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1; 
+    int pontosturisticos1;
+
+    //Declaração das variáveis da Carta 2
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2; 
+    int pontosturisticos2;
 
     // Leitura dos dados da Carta 1
     printf("Digite os dados da Carta 1: \n");
     printf("Estado: \n");
-    scanf(" %c", &estado1);
+    scanf("%4s", &estado1);
     printf("Código: \n");
     scanf("%3s", &codigo1);
     printf("Nome da cidade: \n");
@@ -23,18 +37,10 @@ int main() {
     printf("Numero de pontos turisticos: \n");
     scanf("%d", &pontosturisticos);
 
-    // Exibir informações da Carta 1
-    printf("\nCarta 1: \n");
-    printf("\n - Estado: %s \n - Codigo: %s \n - Cidade: %s", estado, codigocarta, cidade);
-    printf("\n - Populacao: %d", populacao);
-    printf("\n - Area: %.2f km²", areakm2);  // Exibindo com 2 casas decimais
-    printf("\n - PIB: %d \n - Numero de pontos turisticos: %d \n", pib, pontosturisticos);
-
-    // Carta 2
-    printf("\nCarta 2: \n");
-
+    // Leitura dos dados da Carta 2
+    printf("Digite os dados da Carta 2: \n");
     printf("Estado: \n");
-    scanf(" %c", &estado2);
+    scanf("%4s", &estado2);
     printf("Código: \n");
     scanf("%3s", &codigo2);
     printf("Nome da cidade: \n");
@@ -49,12 +55,24 @@ int main() {
     printf("Numero de pontos turisticos: \n");
     scanf("%d", &pontosturisticos);
 
-    // Exibir informações da Carta 2
-    printf("\nCarta 2: \n");
-    printf("\n - Estado: %s \n - Codigo: %s \n - Cidade: %s", estado, codigocarta, cidade);
-    printf("\n - Populacao: %d", populacao);
-    printf("\n - Area: %.2f km²", areakm2); // Exibindo com 2 casas decimais
-    printf("\n - PIB: %d \n - Numero de pontos turisticos: %d", pib, pontosturisticos);
+    // Exibir informações das cartas
+    printf("\n - Carta 1: ");
+    printf("\n - Estado: %c", estado1);
+    printf("\n - Código: %s", codigo1);
+    printf("\n - Cidade: %s", cidade1);
+    printf("\n - População: %d", populacao1);
+    printf("\n - Área: %.2f km²", area1);
+    printf("\n - PIB: %.2f", pib1);
+    printf("\n - Número de pontos turísticos: %d \n", pontosturisticos1);
+
+    printf("\n - Carta 2: ");
+    printf("\n - Estado: %c", estado2);
+    printf("\n - Código: %s", codigo2);
+    printf("\n - Cidade: %s", cidade2);
+    printf("\n - População: %d", populacao2);
+    printf("\n - Área: %.2f km²", area2);
+    printf("\n - PIB: %.2f", pib2);
+    printf("\n - Número de pontos turísticos: %d \n", pontosturisticos2);
 
     return 0;
 }
